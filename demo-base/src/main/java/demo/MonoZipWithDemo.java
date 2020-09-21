a05cf50133f4b6ddfdb42413c68d963856464da4
taishicheng <taishicheng@liepin.com>
2020-09-21 15:38:48 +0800

    基础demo

---
+++ demo-base/src/main/java/demo/MonoZipWithDemo.java
@@ -0,0 +1,14 @@
+package demo;
+
+import reactor.core.publisher.Mono;
+
+public class MonoZipWithDemo {
+    public static void main(String[] args) throws InterruptedException {
+        //第一个流程返回空,如果不适用defaultIfEmpty或者switchIfEmpty,流程将直接结束,不再执行后续操作
+        Mono.empty().zipWith(Mono.just("bbb")).doOnNext(objects -> {
+            System.out.println(objects.getT1());
+            System.out.println(objects.getT2());
+        }).subscribe();
+        Thread.sleep(10000L);
+    }
+}
